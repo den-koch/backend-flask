b3371df9ba9a9388e91a7d5e789ba0890e3847c3
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-10-16 22:47:49 +0300

    [update] /healthcheck endpoint

--- flask_app/views.py
+++ flask_app/views.py
@@ -1,6 +1,13 @@
 from flask_app import app
 
+from datetime import datetime
+
 
 @app.route("/healthcheck")
 def healthcheck():
-    return "200"
+    current_datetime = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    response = {
+        "status": "200 OK",
+        "date": current_datetime
+    }
+    return response

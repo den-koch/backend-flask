c053142d116a0010a2d8cb9ab4e8d0a9490a8c38
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-11-19 13:55:36 +0200

    [add] blueprints registration

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -3,3 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 
 import flask_app.views
+
+# app blueprints
+
+from .user import user as user_blueprint
+app.register_blueprint(user_blueprint, url_prefix="/users")
+
+from .category import category as category_blueprint
+app.register_blueprint(category_blueprint, url_prefix="/categories")
+
+from .record import record as record_blueprint
+app.register_blueprint(record_blueprint, url_prefix="/records")

cea8440c3dd3b18eaf4077b7fc347cd570080913
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-12-21 20:40:56 +0200

    [Update] flask app init

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,16 +1,24 @@
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_pyfile("config.py", silent=True)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 import flask_app.views
 
 # app blueprints
 
 from .user import user as user_blueprint
+
 app.register_blueprint(user_blueprint)
 
 from .category import category as category_blueprint
+
 app.register_blueprint(category_blueprint)
 
 from .record import record as record_blueprint
+
 app.register_blueprint(record_blueprint)

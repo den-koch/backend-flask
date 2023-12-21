614c2136906c644250db3e2ec48a7d4a3a0f1cd9
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-12-21 20:30:27 +0200

    [Upgrade] category endpoints

--- flask_app/category/views.py
+++ flask_app/category/views.py
@@ -1,29 +1,53 @@
 from flask import request
-import uuid
+from marshmallow import ValidationError
+from sqlalchemy.exc import NoResultFound, IntegrityError
 
 from . import category
-from ..models import Category
+from flask_app import app, db
+from flask_app.models import CategoryModel
+from flask_app.models.schemas import CategorySchema
 
-categories = {}
+category_schema = CategorySchema()
 
 
 @category.route("/categories", methods=["GET", "POST"])
 def get_create_categories():
     if request.method == "GET":
-        return {category_id: category.__dict__ for category_id, category in categories.items()}
-        # return categories
+        categories = CategoryModel.query.all()
+
+        json_categories = category_schema.dump(categories, many=True)
+
+        return json_categories, 200
 
     if request.method == "POST":
-        category_id = uuid.uuid4().hex
-        new_category = Category(category_id=category_id, **request.get_json())
-        categories[category_id] = new_category
-        return new_category.__dict__
+
+        json_data = request.get_json()
+
+        try:
+            data = category_schema.load(json_data)
+        except ValidationError as err:
+            return err.messages, 400
+
+        post_category = CategoryModel(category_name=data["category_name"])
+
+        with app.app_context():
+            db.session.add(post_category)
+            db.session.commit()
+
+            json_category = category_schema.dump(post_category)
+
+        return json_category, 201
 
 
 @category.route("/categories/<string:category_id>", methods=["DELETE"])
 def delete_category(category_id):
-    if category_id not in categories.keys():
-        return {"status": "204 (NO CONTENT)", "message": "Category not found"}
+    with app.app_context():
+        try:
+            delete_category = CategoryModel.query.filter_by(id=category_id).one()
+        except NoResultFound:
+            return {"message": f"Category {category_id} not found"}, 204
 
-    del categories[category_id]
-    return {"status": "200 (OK)", "message": "Category deleted"}
+        db.session.delete(delete_category)
+        db.session.commit()
+
+        return {"message": f"Category {category_id} deleted"}, 200

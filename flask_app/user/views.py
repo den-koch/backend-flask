ddbb1f15ca9a61eb314ad2876958cba05d0ce56f
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-12-21 20:29:52 +0200

    [Upgrade] user endpoints

--- flask_app/user/views.py
+++ flask_app/user/views.py
@@ -1,34 +1,66 @@
 from flask import request
-import uuid
+from marshmallow import ValidationError
+from sqlalchemy.exc import NoResultFound, IntegrityError
 
 from . import user
-from ..models import User
+from flask_app import app, db
+from flask_app.models import UserModel
+from flask_app.models.schemas import UserSchema
 
-users = {}
+user_request_schema = UserSchema()
+user_response_schema = UserSchema(only=("id", "user_name", "email"))
 
 
 @user.route("/users", methods=["GET"])
 def get_users():
-    return {user_id: user.__dict__ for user_id, user in users.items()}
+    users = UserModel.query.all()
+
+    json_users = user_response_schema.dump(users, many=True)
+
+    return json_users, 200
 
 
 @user.route("/users", methods=["POST"])
 def create_user():
-    user_id = uuid.uuid4().hex
-    new_user = User(user_id=user_id, **request.get_json())
-    users[user_id] = new_user
-    return new_user.__dict__
+    json_data = request.get_json()
+
+    try:
+        data = user_request_schema.load(json_data)
+    except ValidationError as err:
+        return err.messages, 400
+
+    post_user = UserModel(user_name=data["user_name"], email=data["email"], password=data["password"])
+
+    with app.app_context():
+        db.session.add(post_user)
+        db.session.commit()
+
+        json_user = user_response_schema.dump(post_user)
+
+        return json_user, 201
 
 
 @user.route("/users/<string:user_id>", methods=["GET", "DELETE"])
 def get_delete_user(user_id):
     if request.method == "GET":
-        if user_id not in users.keys():
-            return {"status": "404 (NOT FOUND)", "message": "User not found"}
-        return {user_id: users[user_id].__dict__}
+
+        try:
+            get_user = UserModel.query.filter_by(id=user_id).one()
+        except NoResultFound:
+            return f"User {user_id} not found", 404
+
+        json_user = user_response_schema.dump(get_user)
+
+        return json_user, 200
 
     if request.method == "DELETE":
-        if user_id not in users.keys():
-            return {"status": "204 (NO CONTENT)", "message": "User not found"}
-        del users[user_id]
-        return {"status": "200 (OK)", "message": "User deleted"}
+        with app.app_context():
+            try:
+                delete_user = UserModel.query.filter_by(id=user_id).one()
+            except NoResultFound:
+                return f"User {user_id} not found", 204
+
+            db.session.delete(delete_user)
+            db.session.commit()
+
+        return f"User {user_id} deleted", 200

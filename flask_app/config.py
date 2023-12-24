a9d156260bea96cbb7db2181713eb6f39e3505bb
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-12-24 21:09:21 +0200

    [Update] config db

--- flask_app/config.py
+++ flask_app/config.py
@@ -3,8 +3,7 @@ import os
 PROPAGATE_EXCEPTIONS = True
 FLASK_DEBUG = True
 JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY')
-# SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@db:5432/{os.getenv('POSTGRES_DB')}"
-SQLALCHEMY_DATABASE_URI = "postgresql://flask_db_yv6s_user:hMOEuttjIVQti4eEfCIpEOaM0AIp5VZ8@dpg-cm28f7da73kc738kg990-a.oregon-postgres.render.com/flask_db_yv6s"
+SQLALCHEMY_DATABASE_URI = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@{os.getenv('POSTGRES_HOST')}/{os.getenv('POSTGRES_DB')}"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 API_TITLE = "Flask home accounting REST API"
 API_VERSION = "v3"

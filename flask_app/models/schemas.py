6de515fd2c8f66120dbac02afb809f7f5bd6de5a
Denys Kochetov <125379298+den-koch@users.noreply.github.com>
2023-12-24 19:09:18 +0200

    [Update] schemas to jwt req

--- flask_app/models/schemas.py
+++ flask_app/models/schemas.py
@@ -1,22 +1,17 @@
 from marshmallow import Schema, fields, validate, validates, ValidationError
-from flask_app.models import UserModel, CategoryModel
+from flask_app.models import CategoryModel
 
 
 class UserSchema(Schema):
     id = fields.UUID(dump_only=True)
     user_name = fields.String(required=True)
-    email = fields.Email(required=True)
+    email = fields.Email(required=True, validate=validate.Email())
     password = fields.String(required=True, load_only=True)
-
-    @validates("email")
-    def validate_email(self, email):
-        if UserModel.query.filter_by(email=email).first() is not None:
-            raise ValidationError("Email already exists")
 
 
 class RecordSchema(Schema):
     id = fields.UUID(dump_only=True)
-    user_id = fields.UUID(required=True)
+    # user_id = fields.UUID(required=True)
     category_id = fields.UUID(required=True)
     date_time = fields.DateTime(required=True)
     amount = fields.Float(required=True, validate=validate.Range(min=0.0))
@@ -28,12 +23,12 @@ class CategorySchema(Schema):
     is_custom = fields.Boolean(dump_only=True)
 
     @validates("category_name")
-    def validate_email(self, category_name):
+    def validate_category_name(self, category_name):
         if CategoryModel.query.filter_by(category_name=category_name).first() is not None:
             raise ValidationError("Category already exists")
 
 
 class UserCategorySchema(Schema):
     id = fields.UUID(dump_only=True)
-    user_id = fields.UUID(required=True)
+    # user_id = fields.UUID(required=True)
     category_id = fields.UUID(required=True)
